769f0931a176be513739d08cebb08c302507064b
luanadefourny <luanadefourny@gmail.com>
2025-07-23 23:03:48 +0100

    added support for jsdocs

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 const DB_PORT = process.env.DB_PORT || 27017;
-const DB_NAME = process.env.DB_NAME || 'books';
+const DB_NAME = process.env.DB_NAME || 'bria';
 
 (async() => {
   try {
