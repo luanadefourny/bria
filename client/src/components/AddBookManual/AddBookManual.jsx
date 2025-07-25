184735dd08243db6fd738594ff179bf0e9244173
luanadefourny <luanadefourny@gmail.com>
2025-07-25 18:00:35 +0100

    add by manual adds to database correctly, but plenty of missing data

--- client/src/components/AddBookManual/AddBookManual.jsx
+++ client/src/components/AddBookManual/AddBookManual.jsx
@@ -1,28 +1,105 @@
+import { useState } from 'react';
 import './../AddBook/AddBook.css';
+import { postBook } from '../../services/bookService';
 
 function AddBookManual () {
+
+  const [title, setTitle] = useState('');
+  const [authors, setAuthors] = useState([]);
+  const [isbn, setIsbn] = useState('');
+  const [publishedDate, setPublishedDate] = useState('');
+  const [bookData, setBookData] = useState({
+    title: title,
+    authors: authors,
+    isbn: isbn,
+    publishedDate: publishedDate
+  });
+
+  async function handleSubmit (e) {
+    e.preventDefault();
+    try {
+      setBookData({
+        title: title,
+        authors: authors,
+        isbn: isbn,
+        publishedDate: publishedDate
+      })
+      const newBook = await postBook(bookData)
+      console.log('new book manual: ', newBook);
+      //reset everything
+      setTitle('');
+      setAuthors([]);
+      setIsbn('');
+      setPublishedDate('');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //TODO: could refactor to make one function that also takes the set function as parameter
+  function handleTitleChange (e) {
+    const str = e.target.value;
+    setTitle(str);
+  }
+  function handleAuthorsChange (e) {
+    const arr = e.target.value;
+    setAuthors(arr);
+  }
+  function handleIsbnChange (e) {
+    const str = e.target.value;
+    setIsbn(str);
+  }
+  function handlePublishedDateChange (e) {
+    const str = e.target.value;
+    setPublishedDate(str);
+  }
+
   return (
-    <form className="addbook-form-container manual-form">
+    <form 
+      className="addbook-form-container manual-form"
+      onSubmit={handleSubmit} >
       <div className="addbook-form-input-container">
         <label className="form-input-label">title</label>
-        <input className="form-input-input" type="text" placeholder="book title" required />
+        <input 
+          className="form-input-input" 
+          type="text" 
+          placeholder="book title" 
+          required
+          value={title}
+          onChange={handleTitleChange} />
       </div>
       <div className="addbook-form-input-container">
         <label className="form-input-label">author</label>
-        <input className="form-input-input" type="text" placeholder="author(s)" required />
+        <input 
+          className="form-input-input" 
+          type="text" 
+          placeholder="author(s)" 
+          required 
+          value={authors}
+          onChange={handleAuthorsChange} />
       </div>
       <div className="addbook-form-input-container">
         <label className="form-input-label">isbn</label>
-        <input className="form-input-input" type="text" placeholder="isbn" />
+        <input 
+          className="form-input-input" 
+          type="text" 
+          placeholder="isbn"
+          value={isbn}
+          onChange={handleIsbnChange} />
       </div>
       <div className="addbook-form-input-container">
         <label className="form-input-label">published year</label>
-        <input className="form-input-input" type="number" placeholder="year" />
+        <input 
+          className="form-input-input" 
+          type="text" 
+          placeholder="year"
+          value={publishedDate}
+          onChange={handlePublishedDateChange} />
       </div>
-      <div className="addbook-form-input-container">
+      {/* <div className="addbook-form-input-container">
         <label className="form-input-label"></label>
         <input className="form-input-input" />
-      </div>
+      </div> */}
       <button type="submit">Add</button>
     </form>
   );
--- client/src/services/bookService.js
+++ client/src/services/bookService.js
@@ -29,29 +29,55 @@ export { postBook };
 
 //helper function to get additional book data and rebuild book object
 async function buildBookObject (book) {
-  const editionKey = book.cover_edition_key;
+  console.log('book in build object at beginning: ', book);
+  const editionKey = book.cover_edition_key || '';
   console.log('editionkey: ',editionKey);
-  const worksKey = book.key.split('/').pop();
+  const worksKey = book.key?.split('/').pop() || '';
   console.log('workskey: ',worksKey);
 
   let editionData = {};
   let worksData = {};
 
-  if (editionKey) editionData = await getBookByEditionKey(editionKey);
-  if (worksKey) worksData = await getBookByWorksKey(worksKey);
-
+  if (editionKey) {
+    try {
+      editionData = await getBookByEditionKey(editionKey);
+    } catch (error) {
+      console.log(error);
+    } 
+  }
+  if (worksKey) {
+    try {
+      worksData = await getBookByWorksKey(worksKey);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   console.log('edition book data: ',editionData);
   console.log('works book data: ', worksData);
 
+
+  console.log('title: ',book.title);
+  console.log('author: ',book.author_name || book.authors);
+  console.log('isbn: ',editionData.isbn_13?.[0] || editionData.isbn_10?.[0] || '');
+  console.log('editionkey: ',editionKey || '');
+  console.log('workskey: ',worksKey || '');
+  console.log('pages: ',editionData.number_of_pages || null);
+  console.log('description: ', worksData?.description?.value || '');
+  console.log('published date: ',book.publishedDate || book.first_publish_year
+      ? new Date(`${book.first_publish_year}-01-01`)
+      : null);
+  console.log('cover: ',book.cover_i || null);
+
+
   const newBook = {
     title: book.title,
-    authors: book.author_name,
+    authors: book.author_name || book.authors,
     isbn: editionData.isbn_13?.[0] || editionData.isbn_10?.[0] || '',
-    editionKey: editionKey,
-    worksKey: worksKey,
+    editionKey: editionKey || '',
+    worksKey: worksKey || '',
     pages: editionData.number_of_pages || null,
-    description: worksData.description.value || '',
-    publishedDate: book.first_publish_year
+    description: worksData?.description?.value || '',
+    publishedDate: book.publishedDate || book.first_publish_year
       ? new Date(`${book.first_publish_year}-01-01`)
       : null,
     cover: book.cover_i || null,
